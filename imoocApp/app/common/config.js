72a452f468f4af2261dd5f2d0ffdb460b9934e8c
sean <eli01linux@aliyun.com>
2017-05-12 22:32:34 +0800

    将请求相关的请求 URL 和请求头等放在配置文件中

---
+++ imoocApp/app/common/config.js
@@ -0,0 +1,13 @@
+export default {
+	header: {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		}
+	},
+	api: {
+		base: 'http://rapapi.org/mockjs/18917/',
+		creations: 'api/creations'
+	}
+}
