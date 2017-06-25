07493bb722c7daaaa160c705aef33ed158b9ce1a
孟庆申 <mengqingshen@meituan.com>
2017-06-25 21:15:02 +0800

    link react-native-audio react-native-sound

--- imoocApp/android/app/src/main/java/com/imoocapp/MainApplication.java
+++ imoocApp/android/app/src/main/java/com/imoocapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.imoocapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
+import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSoundPackage(),
+            new ReactNativeAudioPackage(),
             new ImagePickerPackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage()
